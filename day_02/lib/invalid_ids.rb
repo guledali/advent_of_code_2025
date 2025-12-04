94f217903dd45fa8a230e2e46c72051636d5dca9
guledali <guled.ali01@gmail.com>
2025-12-04 22:52:22 +0100

    Add more the case

--- day_02/lib/invalid_ids.rb
+++ day_02/lib/invalid_ids.rb
@@ -10,13 +10,26 @@ class InvalidIds
   def find(range)
     range.each do |num|
       digits = num.to_s.chars
-      @invalid_ids.push(num) if digits.first == digits.last
+
+      repeated_twice(num, digits)
     end
 
     get_length
   end
 
   private
+
+  def repeated_twice(num, digits)
+    if digits.length == 2
+      @invalid_ids.push(num) if digits.first == digits.last
+    elsif digits.length == 3
+      first = digits[0]
+      second = digits[1]
+      third = digits[2]
+
+      @invalid_ids.push(num) if first == second && second == third
+    end
+  end
 
   def get_length
     @invalid_ids.length
--- day_02/test/invalid_ids_test.rb
+++ day_02/test/invalid_ids_test.rb
@@ -6,11 +6,19 @@ require_relative '../lib/invalid_ids'
 
 # Tests for InvalidIDS, checking repeated-IDs in ranges.
 class TestInvalidIDs < Minitest::Test
-  def test_invalid_ids
+  def test_invalid_ids_first
     range = 10..23
 
     invalid_ids = InvalidIds.new
 
     assert_equal 2, invalid_ids.find(range)
+  end
+
+  def test_invalid_ids_second
+    range = 95..115
+
+    invalid_ids = InvalidIds.new
+
+    assert_equal 2, invalid_ids.find(range) # 99
   end
 end
