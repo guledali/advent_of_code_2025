2fdf9b87e9d729c6403d0182e206d16e0d944af5
guledali <guled.ali01@gmail.com>
2025-12-20 20:08:46 +0100

    Most of day 5

--- day_05/lib/cafeteria.rb
+++ day_05/lib/cafeteria.rb
@@ -3,11 +3,32 @@
 
 # Class for Cafeteria
 class Cafeteria
+  attr_reader :available_ingredient_ids, :ingredient_id_ranges
+
+  def initialize
+    @ingredient_id_ranges = Cafeteria.data.select { |line| line.include?('-') } || []
+    @available_ingredient_ids = (Cafeteria.data - @ingredient_id_ranges).reject(&:empty?) || []
+  end
+
   def self.data
     File.read(File.join(__dir__, '../fixtures', 'data.txt')).split("\n")
   end
 
-  def available_ingredient
-    [1, 5, 8, 11, 17, 32]
+  def data?
+    Cafeteria.data.any?
+  end
+
+  def find_ingredient(ranges:, ids:)
+    fresh_ingredients = []
+
+    ids.each do |id|
+      fresh_ingredients << id if ranges.include?(id)
+    end
+
+    fresh_ingredients
+  end
+
+  def to_range(range)
+    Range.new(*range.split('-').map(&:to_i)).to_a # '3-5' => 3,4,5
   end
 end
--- day_05/test/cafeteria_test.rb
+++ day_05/test/cafeteria_test.rb
@@ -4,16 +4,37 @@
 require 'minitest/autorun'
 require_relative '../lib/cafeteria'
 
-describe 'day_05' do
-  describe 'cafeteria' do
-    it 'should read the data file correctly' do
-      refute Cafeteria.data.empty?
-    end
+describe 'Cafeteria' do
+  def setup
+    @cafeteria = Cafeteria.new
+  end
 
-    it 'should find all the available_ingredient in the data' do
-      cafeteria = Cafeteria.new
+  it 'should read the data file correctly' do
+    assert_equal true, @cafeteria.data?
+  end
 
-      assert_equal [1, 5, 8, 11, 17, 32], cafeteria.available_ingredient
-    end
+  it 'should find all the available_ingredient in the data' do
+    available_ingredient = [1, 5, 8, 11, 17, 32].map(&:to_s)
+
+    assert_equal available_ingredient, @cafeteria.available_ingredient_ids
+  end
+
+  it 'should be find all the fresh ID ranges in the data,' do
+    id_ranges = %w[
+      3-5
+      10-14
+      16-20
+      12-18
+    ]
+
+    assert_equal id_ranges, @cafeteria.ingredient_id_ranges
+  end
+
+  it 'should return all numbers in the given range' do
+    assert_equal [3, 4, 5, 6, 7], @cafeteria.to_range('3-7')
+  end
+
+  it 'should find the fresh ingredients in the available' do
+    assert_equal [4, 5], @cafeteria.find_ingredient(ranges: [4, 5, 6, 7, 8], ids: [4, 5])
   end
 end
